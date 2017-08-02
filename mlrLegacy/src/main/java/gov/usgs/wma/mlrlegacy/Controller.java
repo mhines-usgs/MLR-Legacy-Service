6fcb8755b27cc3fa10af02322dfdc73fcdc92954
Mary Bucknell <mbucknell@usgs.gov>
2017-08-02 10:39:33 -0500

    Stubbed out Post monitoringLocations

--- mlrLegacy/src/main/java/gov/usgs/wma/mlrlegacy/Controller.java
+++ mlrLegacy/src/main/java/gov/usgs/wma/mlrlegacy/Controller.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -21,6 +23,11 @@ public class Controller {
     @GetMapping("/{id}")
     public MonitoringLocation getMonitoringLocation(@PathVariable("id") String id) {
         return new MonitoringLocation();
+    }
+    
+    @PostMapping()
+    public MonitoringLocation createMonitoringLocation(@RequestBody MonitoringLocation ml) {
+        return ml;
     }
     
 }
