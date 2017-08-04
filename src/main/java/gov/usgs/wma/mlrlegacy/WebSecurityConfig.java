77e033e2ccc9f7cf349be01081de564863524163
Steinich, Dave R. <drsteini@usgs.gov>
2017-08-04 11:19:31 -0500

    Authentication for monitoring endpoints, one automated test, more config via environment variables

---
+++ src/main/java/gov/usgs/wma/mlrlegacy/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package gov.usgs.wma.mlrlegacy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${mlrLegacyServicePwd}")
+	private String pwd;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/monitoringLocations/**").permitAll()
+				.antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/**").permitAll()
+				.antMatchers("/health/**").permitAll()
+				.anyRequest().fullyAuthenticated()
+			.and()
+				.formLogin().permitAll()
+			.and()
+				.logout().permitAll()
+		;
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.inMemoryAuthentication()
+		.withUser("user").password(pwd).roles("ACTUATOR");
+	}
+
+}
