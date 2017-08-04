173c89bfeddd98c550e2a9b7f7524b13b742b4ea
Steinich, Dave R. <drsteini@usgs.gov>
2017-08-04 10:28:45 -0500

    whitespace wars

--- src/main/java/gov/usgs/wma/mlrlegacy/MonitoringLocationDao.java
+++ src/main/java/gov/usgs/wma/mlrlegacy/MonitoringLocationDao.java
@@ -3,33 +3,34 @@ package gov.usgs.wma.mlrlegacy;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
-import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MonitoringLocationDao {
-    
-    private final SqlSession sqlSession;
-    
-    public MonitoringLocationDao(SqlSession sqlSession) {
-        this.sqlSession = sqlSession;
-    }
-    
-    public List<MonitoringLocation> getByMap(Map<String, String> queryParams) {
-        return sqlSession.selectList("getByMap", queryParams);
-    }
-    
-    public MonitoringLocation getById(BigInteger id) {
-        return sqlSession.selectOne("getById", id);
-    }
-    
-    public BigInteger create(MonitoringLocation ml) {
-        sqlSession.insert("create", ml);
-        return ml.getId();
-    }
-    
-    public void update(MonitoringLocation ml) {
-        sqlSession.update("update", ml);
-    }
+
+	private final SqlSession sqlSession;
+
+	public MonitoringLocationDao(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	public List<MonitoringLocation> getByMap(Map<String, String> queryParams) {
+		return sqlSession.selectList("getByMap", queryParams);
+	}
+
+	public MonitoringLocation getById(BigInteger id) {
+		return sqlSession.selectOne("getById", id);
+	}
+
+	public BigInteger create(MonitoringLocation ml) {
+		sqlSession.insert("create", ml);
+		return ml.getId();
+	}
+
+	public void update(MonitoringLocation ml) {
+		sqlSession.update("update", ml);
+	}
+
 }
