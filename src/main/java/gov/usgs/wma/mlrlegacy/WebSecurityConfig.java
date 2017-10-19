511ef48b17e9267a51d847035010b8d61da43447
Steinich, Dave R. <drsteini@usgs.gov>
2017-10-19 08:59:27 -0500

    Working example with just config

--- src/main/java/gov/usgs/wma/mlrlegacy/WebSecurityConfig.java
+++ src/main/java/gov/usgs/wma/mlrlegacy/WebSecurityConfig.java
@@ -3,12 +3,13 @@ package gov.usgs.wma.mlrlegacy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableResourceServer
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
@@ -18,6 +19,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/**").permitAll()
 				.antMatchers("/health/**").permitAll()
 				.anyRequest().fullyAuthenticated()
+			.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 				.cors()
 			.and()
